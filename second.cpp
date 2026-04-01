050a35de92bdb9118489755094a3a0e24005970c
Muhammad Ali <alihashar0406@gmail.com>
2026-03-31 21:48:28 -0400

    inital

---
+++ second.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main(){
+    //int x;
+    int age=15;
+    double hieght=100.244;
+    std::string name= "ali";
+    std::cout<<"hello "<<name<<"you are"<<age<<"years old"<<'\n';
+    std:: cout<<"and you hieght is"<<hieght;
+}
---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// this is a comment
+/*
+this 'is 
+mutly line 
+comment
+*/
+int main(){
+    std::cout<<"Mashallah Ali"<<std::endl;
+    std::cout<<"May Allah guide you"<< '\n';
+    return 0;
+}
